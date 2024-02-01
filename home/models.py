09fa147d8e9bb3adf0e338ea7aeeb697a1b68506
gvarshithreddy <gvarshithreddy8@gmail.com>
2024-02-01 09:57:42 +0530

    added announcement page and startedapp called members

--- home/models.py
+++ home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Announcement(models.Model):
+    title = models.CharField(max_length=100)
+    new = models.BooleanField(default=True)
+    body = models.TextField()
+    date = models.DateTimeField()
+    # def save(self, *args, **kwargs):
+    #     ''' On save, update timestamps '''
+    #     self.date = timezone.now()
+    #     return super(Announcement, self).save(*args, **kwargs)
+    def __str__(self):
+        return self.title
--- home/views.py
+++ home/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, loader
+from django.http import HttpResponse
+from .models import Announcement
 
 # Create your views here.
 
 def home(request):
     return render(request, 'main.html')
+
+def announcements(request):
+    template = loader.get_template('announcements.html')
+    context = {
+        'announcements': Announcement.objects.all().order_by('-date'),
+    }
+
+    return HttpResponse(template.render(context, request))
