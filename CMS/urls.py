57aa76d99e8133664d85af93354b6d70402602f2
gvarshithreddy <gvarshithreddy8@gmail.com>
2024-02-01 10:40:29 +0530

    added login page but not functionality

--- CMS/urls.py
+++ CMS/urls.py
@@ -19,5 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('', include('home.urls')),
+    path('', include('members.urls')),
     path('admin/', admin.site.urls),
 ]
--- members/views.py
+++ members/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, user_logged_out
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib import messages
+
+
 
 # Create your views here.
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # authenticate user
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You have successfully logged in!')
+            return HttpResponseRedirect('/')
+        else:
+            messages.error(request, 'Error logging in. Please try again.')
+            return HttpResponseRedirect('/login')
+    else:
+        return render(request, 'login.html')
